ace0ce4761cf917f563e836f2ebb8b545d87edfa
gkgotkorawit-crypto <gk.gotkorawit@gmail.com>
2026-09-17 07:20:50 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "bingx-profit-v14";
+const CACHE = "bingx-profit-v16";
 const ASSETS = ["./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
